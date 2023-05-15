e099df6e92db16989b254f3c595df713fab46294
khraina <84083826+khraina@users.noreply.github.com>
2023-05-16 00:30:07 +0530

    backend not connected

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,session,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
 
 
 
+db=SQLAlchemy()
+DB_NAME= 'database.db'
 app = Flask(__name__)
+app.config['SECRET_KEY']='bene'
+app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
+
+adminkey='z'
+
+username=''
+
+def create_database(app):
+    db.init_app(app)
+    if not path.exists(DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print("Database creaated")
+        
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String,unique=True)
+    fullName=db.Column(db.String(100))
+    password=db.Column(db.String(100))  
+    
+    
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 @app.route("/")
@@ -27,15 +60,42 @@ def contact():
 def logins():
   return render_template('logins.html')
 
-@app.route("/logint")
+@app.route("/logint", methods=['GET', 'POST'])
 def logint():
+  if request.method == 'POST':
+    username= request.form['email']
+    password = request.form['password']
+    user = User.query.filter_by(username=username).first()
+    if user:
+        if user.password == password:
+          login_user(user, remember=True)
+          return redirect('/dash')
+        else:
+          print('Wrong pass')
   return render_template('logint.html')
 
-@app.route("/register")
+@app.route("/register", methods=["POST", "GET"])
 def register():
-  return render_template('register.html')
+  if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email   = request.form['email']
+        adminkey = request.form['adminkey']
+        if adminkey != 'aisat123':
+            return redirect(url_for('logint'))   
+        new_user=User(fullName=username,password=password,email=email)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('logint'))
 
+  else:
+      return render_template('register.html')
 
+@app.route('/logout')
+@login_required
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
 
 # main driver function
 if __name__ == '__main__':
